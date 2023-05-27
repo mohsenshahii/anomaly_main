96419f69da881add77b9e199f784ee4b016c3af0
mohsen <mohsen1370shahi@gmail.com>
2023-05-27 17:27:59 +0200

    I did anomaly detection using aoutoencoder on real data relating to fraud detection and successfuly obtained outcome, in the next stage I'll change the code a little inorder to show how precise the method is

--- ano_det.py
+++ ano_det.py
@@ -8,10 +8,10 @@ from tensorflow.keras import layers
 import matplotlib.pyplot as plt
 
 # Load the real data from a CSV file
-data = pd.read_csv('your_data.csv')
+data = pd.read_csv('creditcard.csv')
 
 # Extract the features from the data (assuming all columns except the target variable are features)
-features = data.drop('target_variable', axis=1)
+features = data.drop('Class', axis=1)
 
 # Split the data into training and testing sets
 train_data, test_data = train_test_split(features, test_size=0.2, random_state=42)
@@ -44,14 +44,14 @@ reconstructed_data = model.predict(test_data)
 mse = np.mean(np.power(test_data - reconstructed_data, 2), axis=1)
 
 # Define a threshold for anomaly detection (e.g., using mean + 3 * standard deviation)
-threshold = np.mean(mse) + 3 * np.std(mse)
+threshold = np.mean(mse) + 6* np.std(mse)
 
 # Identify anomalies
 anomalies = test_data[mse > threshold]
 
 # Plot the data
-plt.scatter(test_data[:, 0], test_data[:, 1], color='blue', label='Normal')
-plt.scatter(anomalies[:, 0], anomalies[:, 1], color='red', label='Anomaly')
+plt.scatter(test_data[:, 20], test_data[:, 24], color='blue', label='Normal')
+plt.scatter(anomalies[:, 20], anomalies[:, 24], color='red', label='Anomaly')
 plt.xlabel('Feature 1')
 plt.ylabel('Feature 2')
 plt.title('Anomaly Detection using Autoencoders')
